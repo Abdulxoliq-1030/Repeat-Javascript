2af653b1c2d7e8ba8eff1a24a1fcf59ea2f623ec
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-09-27 18:56:56 +0500

    Reference types. Object

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// Javascript da Ikki xil turdagi o'zgaruvchilar bor Primitive and Reference
+// Javascript dagi Reference turlar => Object, Array, Function
 
-// Primitive types: String, Number, Boolean, undefined, null
+let person = {
+  name: "John",
+  age: 34,
+};
 
-let name = "John"; // String
-let age = 13; // Number
-let isMarried = false; // Boolean
-let firstName = undefined; // undefined if some variables are met but it doesn't have value this variable get undefined
-let selectedColor = null; // null agar bironta mijoz kiyimni rangini tanlamoqchi bo'lsa boshida uning rangi yo'q bo'ladi shu holatda yo'q bo'lib turishi uchun null ishlatiladi. Asosan o'zgaruvchini qiymatini tozalab tashlash uchun ishlatiladi.
-
-// Dasturlash tillari ikki xil bo'ladi static va dynamic
-// Static dasturlash tillarida o'zgaruvchi berilayotgan vaqtida uning typi beriladi dynamic tillarda esa runtime vaqtida bilinadi dynamic tillar typini runtime vaqtida aniqlab oladi javascript tili ham dynamic til xisoblanadi
-
-
+// Javascriptda objectning qiymatini olishi uchun . notation va scope notation ishlatiladi
+console.log(person.name); //. notation
+console.log(person["name"]); // scope notation
+console.log(person);
