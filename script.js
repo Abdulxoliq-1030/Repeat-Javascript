0370aed10d35c79234bd655827727e3288544d89
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-09-27 18:03:04 +0500

    javascript variables and constants

---
+++ script.js
@@ -0,0 +1,18 @@
+let name;
+console.log(name); // undefined
+
+let name1 = "John";
+
+// 1. Kalit so'z bo'lmasligi kerak
+// let,for,it,else...
+// 2. Raqam bilan boshlanishi mumkin emas
+// 1name, 76firstName
+// 3. Probel yoki chiziqcha ishlatish ham mumkin emas
+// 4. Case-sensitive hisoblanadi
+// 5.Mazmunli bo'lishi kerak
+// x, abc, myVar they names not best practise
+
+const pi = 3.14; // we never change this value because it's constant
+console.log(pi);
+
+
