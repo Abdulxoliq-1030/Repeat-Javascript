11cc5c101231584932ac54855535058e29af5366
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-09-27 19:02:38 +0500

    Reference types. Arrays

---
+++ index.js
@@ -0,0 +1,6 @@
+// Arrays
+let selectedColors = ["white", "green"];
+selectedColors[2] = 7;
+console.log(selectedColors.length);
+
+// Massivning turi ham object hisoblanadi va uning ichidagi qiymatlari ham dynamic hisoblanadi va uning o'lchami ham dynamic
