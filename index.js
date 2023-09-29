ff81e0fd22bfd6221cffc31b84e6d2a9f406b923
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-09-29 20:54:00 +0500

    constructor

--- index.js
+++ index.js
@@ -16,11 +16,16 @@ function Circle(radius) {
   };
 }
 
-const circle = new Circle(1);
-circle.color = "green";
-circle.move = function () {
-  // ...
-};
-delete circle.color;
+// const circle = new Circle(1);
+// circle.color = "green";
+// circle.move = function () {
+//   // ...
+// };
 
-console.log(circle);
+const circle1 = createCircle(1);
+const circle2 = new Circle(1);
+
+console.log(circle1.constructor);
+console.log(circle2.constructor);
+
+// Har qanday objectning konstructor hossasi bo'ladi bu konstructor bu objectning asosiy objectiga yo'l ko'rsatib beradi
