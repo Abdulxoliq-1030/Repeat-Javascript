f18738d23c3863c3fe35bb9fcd7c2cb48af16402
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-09-29 20:36:02 +0500

    for in and for of

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-let i = 0;
+// For in and For of
 
-while (i <= 10) {
-  if (i % 2 !== 0) {
-    i++;
-    continue;
-  }
-  console.log(i);
-  i++;
+// For in => Object ni hossalari bo'ylab aylanadi
+// For of => Objectni elementlari bo'ylab yurib chiqadi
+
+const person = {
+  name: "Jamol",
+  age: 37,
+};
+
+for (let key in person) {
+  console.log(key, person[key]);
 }
 
-// break loop dan chiqarib yuboradi
-// continue o'tkazib yuboradi
+let colors = ["white", "black", "green", "blue"];
+
+for (let index in colors) {
+  console.log(index, colors[index]);
+}
+
+for (let color of colors) {
+  console.log(color);
+} // this useful array elements
