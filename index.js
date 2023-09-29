ad74b811458b529f01e12a2e4f7f5fe6056c8cda
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-09-29 20:25:56 +0500

    while and do while

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-// for loop
+// while and do while
+let i = 7;
 
-// for (let i = 1; i <= 5; i++) {
-//   if (i % 2 !== 0) {
-//     console.log(i);
-//   }
-// }
+while (i <= 5) {
+  if (i % 2 !== 0) console.log(i);
+  i++;
+} // bunda shart loop boshlanishidan oldin tekshiriladi
 
-for (let i = 1; i < 100; i *= 3) {
-  console.log(i);
-}
+let j = 0;
+do {
+  if (j % 2 !== 0) console.log(j);
+  j++; // infinity loop if we remove this item
+} while (j <= 5); // avval kod birmarta ishga tushadi keyin shart bajariladi
