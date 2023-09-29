63fc128b058718dc0c34dd7cd4af0c13b16bacd0
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-09-29 20:29:12 +0500

    break and continue

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-// while and do while
-let i = 7;
+let i = 0;
 
-while (i <= 5) {
-  if (i % 2 !== 0) console.log(i);
+while (i <= 10) {
+  if (i % 2 !== 0) {
+    i++;
+    continue;
+  }
+  console.log(i);
   i++;
-} // bunda shart loop boshlanishidan oldin tekshiriladi
+}
 
-let j = 0;
-do {
-  if (j % 2 !== 0) console.log(j);
-  j++; // infinity loop if we remove this item
-} while (j <= 5); // avval kod birmarta ishga tushadi keyin shart bajariladi
+// break loop dan chiqarib yuboradi
+// continue o'tkazib yuboradi
