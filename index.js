ec003e876d92ef660b7f9dc2f68e80f6fb210e8d
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-09-30 12:52:20 +0500

    Promise

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-// This method ichida ishlatilsa this o'sha method turgan objectni ifodalaydi agar tashqarida tursa u global objectni ifodalaydi
-function Game(title) {
-  this.title = title;
-  console.log(this);
-}
+// Promise
+// Promise deb kelajakda biron qiymat qaytaruvchi objectga aytiladi
+// Agar funksiya success bo'lsa bizga qiymatni qaytarib beradi
+// Agar bironta hatolik bo'lsa ish vaqtida Promise bizga reasonni qaytarib beradi nima bo'lganini
 
-const roblox = new Game("roblox");
+// Promise 3 xil holatda bo'ladi Pending, Fulfilled with a value, Rejected with a reason
 
-// If this create a new object,this express a new object
+let isBookshopOpen = false;
+// Promise
+let willIGetNewBook = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    if (isBookshopOpen) {
+      let book = {
+        title: "The BIG book of knowledge",
+        author: "Brown Watson",
+      };
+      resolve(book); // fullfilled
+    } else {
+      let reason = new Error("Kitob do'koni yopiq");
+      reject(reason); // reject
+    }
+  }, 1500);
+});
 
-const game = {
-  title: "needforspeed",
-  cars: ["ferrari", "lamborghini", "porsche", "bmw"],
-  showCars() {
-    this.cars.forEach((car) => {
-      console.log(this.title, car);
-    });
-  },
-};
+willIGetNewBook
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error.message));
 
-game.showCars();
+// Promise ning ikkita xossasi bo'ladi state bilan result.
+// State boshida pending bo'lib turadi
+// Result esa undefined bo'ladi
+
+// State keyin fulfilled
+// Result keyin value
+
+// State keyin rejected
+// Result keyin error
