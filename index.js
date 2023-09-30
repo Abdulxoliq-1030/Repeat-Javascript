dcde1fe3a99700a6fbb67e3107209e9cdca05513
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-09-30 07:44:10 +0500

    string methods

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-const circle = {
-  radius: 1,
-  draw() {
-    console.log("doira");
-  },
-};
+// console.log(Math.round(4.3)); // 4
+// console.log(Math.pow(8, 2)); // 64
+// console.log(Math.random()); // return a random number
 
-// const clone = {};
+let greeting = "Hello"; // String primitive
+let greeting2 = new String(); // String object
+console.log(greeting.length);
+console.log(greeting.includes("He")); // true
+console.log(greeting.startsWith("Hello")); // true
+console.log(greeting.endsWith("o")); // true
+console.log(greeting.indexOf("llo")); // 2
+console.log(greeting.replace("Hel", "Sal"));
 
-// for (let key in circle) clone[key] = circle[key];
+const greeting3 = " Hello there ";
 
-// const clone = Object.assign({ color: "white" }, circle);
+console.log(greeting3.trim());
 
-const clone = { ...circle };
+let text = "Salomlashishlil uchun 'Assalom alaykum' deb aytasiz.";
+console.log(text);
 
-console.log(clone);
+console.log(text.split(" "));
+
+let text2 = `this is my
+'first' message`;
+
+console.log(text2);
+
+// String objectlari immutable hisoblanadi string metodlarini ishlashi natijasida yangi string qaytariladi
