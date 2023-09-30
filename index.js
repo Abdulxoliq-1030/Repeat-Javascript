ed62db8b9b07cebc284f841ea9eedadc233bf072
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-09-30 13:00:59 +0500

    callback

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
-// Promise
-// Promise deb kelajakda biron qiymat qaytaruvchi objectga aytiladi
-// Agar funksiya success bo'lsa bizga qiymatni qaytarib beradi
-// Agar bironta hatolik bo'lsa ish vaqtida Promise bizga reasonni qaytarib beradi nima bo'lganini
+function add(a, b, callback) {
+  console.log(`${a} va ${b} larning yig'indisi ${a + b}`);
+  callback();
+}
 
-// Promise 3 xil holatda bo'ladi Pending, Fulfilled with a value, Rejected with a reason
+function display() {
+  console.log(`Bu qator qo'shuvdan keyin chiqadi.`);
+}
 
-let isBookshopOpen = false;
-// Promise
-let willIGetNewBook = new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    if (isBookshopOpen) {
-      let book = {
-        title: "The BIG book of knowledge",
-        author: "Brown Watson",
-      };
-      resolve(book); // fullfilled
-    } else {
-      let reason = new Error("Kitob do'koni yopiq");
-      reject(reason); // reject
-    }
-  }, 1500);
-});
-
-willIGetNewBook
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error.message));
-
-// Promise ning ikkita xossasi bo'ladi state bilan result.
-// State boshida pending bo'lib turadi
-// Result esa undefined bo'ladi
-
-// State keyin fulfilled
-// Result keyin value
-
-// State keyin rejected
-// Result keyin error
+add(3, 4, display);
