d0ec1d7fa06bbc858d91c0895b8d44ea18aa0418
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-09-30 09:25:28 +0500

    every, some, filter methods

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// Array methods 2
+// let numbers = [2, 3, 5, 1, 4];
 
-let first = [1, 2, 3];
-let second = [4, 5, 6];
+// const isAllPositive = numbers.every((number) => {
+//   return number >= 0;
+// });
 
-const concatArrays = first.concat(second); // this combine 2 array return new array and immutable
-console.log(concatArrays);
+// console.log(isAllPositive);
 
-const sliced = concatArrays.slice(2); // [ 3, 4, 5, 6 ] extracts from the given index and returns a new array
-console.log(sliced);
+// let numbers2 = [2, 3, 5, 1, 4];
 
-// Agar objectlar bilan ishlaydigan bo'lsak unda bu methodlar mutable bo'lib qoladi chunki object bo'lsa uning o'ziga emas uning addresini o'zgartiradi
+// const atLeastOnePositive = numbers.some((number) => {
+//   return number >= 0;
+// });
 
-const arr = ["Hello", "Salom", "Hi", "How are you", 5];
+// console.log(atLeastOnePositive);
 
-for (let item of arr) {
-  console.log(item);
-}
+let numbers = [2, 3, -5, 1, 4];
 
-arr.forEach((item, idx) => {
-  // elementni aylanib qaytaradi yangi array qaytarmaydi
-  console.log(item, idx);
-});
+let filtered = numbers.filter((number) => {
+  return number >= 0;
+}); // yangi array qaytaradi
+
+console.log(filtered);
