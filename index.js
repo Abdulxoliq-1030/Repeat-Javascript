dbf100e40dcaeb0a0730236b6b2f0570db939884
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-10-01 19:13:26 +0500

    arrow function

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-async function getCustomerName() {
-  let name = await "Ibrohim";
-  console.log(name);
-  return name;
-}
-
-// getCustomerName().then((data) => console.log(data));
-console.log(1); // 1
-getCustomerName(); // 3
-console.log(2); // 2
+// Arrow funksiyalarni ichida this ishlamaydi
